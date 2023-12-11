b38c76390bf4f96f3ef972ee2c7adc2ae9186264
CalebRosenblum <calebros@umich.edu>
2023-12-11 15:13:00 -0500

    Updated metric calc

--- import sqlite3.py
+++ import sqlite3.py
@@ -29,21 +29,34 @@ def calculate_composite_metric(ppg, games_played, field_goal_percentage, nba_fan
     )
     return composite_metric
 
+import sqlite3
+import matplotlib.pyplot as plt
+
 def fetch_player_data_from_database():
     conn = sqlite3.connect('nba.db')  # Replace with your database name
     cursor = conn.cursor()
+    """
+        ALTER TABLE Players
+        ADD COLUMN SALARY FLOAT;
 
-    # Execute a query to fetch player data from the Players table
+        UPDATE Players
+        SET Players.SALARY = Salaries.SALARY
+        FROM Salaries
+        WHERE Players.PLAYER_ID = Salaries.PLAYER_ID;
+    """
+
+    # Execute a query to fetch player data from the Players table, including salary
     query = """
-        SELECT NAME, FG_PCT, PTS, PFD, GP, REB, AST, STL, BLK, NBA_FANTASY_PTS
-        FROM Players;
+        SELECT p.NAME, p.FG_PCT, p.PTS, p.PFD, p.GP, p.REB, p.AST, p.STL, p.BLK, p.NBA_FANTASY_PTS, s.SALARY
+        FROM Players p
+        INNER JOIN Salaries s ON p.PLAYER_ID = s.PLAYER_ID;
     """
     cursor.execute(query)
 
     # Fetch the data
     player_data = cursor.fetchall()
 
-    # Close the database connection 
+    # Close the database connection
     conn.close()
 
     return player_data
@@ -53,26 +66,23 @@ def main():
 
     # Calculate the composite metric for each player and store in a list
     composite_metrics = []
+    salaries = []
     for player_stats in player_data:
-        player_name, fg_pct, pts, pfd, gp, reb, ast, stl, blk, nba_fantasy_pts = player_stats
+        player_name, fg_pct, pts, pfd, gp, reb, ast, stl, blk, nba_fantasy_pts, salary = player_stats
 
         # Calculate the composite metric for each player
-        composite_metric = calculate_composite_metric(pts, gp, fg_pct, nba_fantasy_pts, reb, ast, blk, stl, pfd)
-        composite_metrics.append((player_name, composite_metric))
+    composite_metric = calculate_composite_metric(
+        pts, gp, fg_pct, nba_fantasy_pts, reb, ast, blk, stl, pfd
+    )
+    composite_metrics.append(composite_metric)
+    salaries.append(salary)
 
-    # Sort the list of tuples based on the composite metric in descending order
-    composite_metrics.sort(key=lambda x: x[1], reverse=True)
-
-    # Extract the top 50 players and their composite metrics for plotting
-    top_50_players = composite_metrics[:50]
-    player_names, composite_metrics_values = zip(*top_50_players)
-
-    # Create a bar graph
+    # Create a scatter plot
     plt.figure(figsize=(12, 8))
-    plt.barh(player_names, composite_metrics_values, color='blue')
-    plt.xlabel('Composite Metric')
-    plt.ylabel('Player Name')
-    plt.title('Top 50 Players by Composite Metric')
+    plt.scatter(salaries, composite_metrics, color='green', alpha=0.7)
+    plt.xlabel('Salary')
+    plt.ylabel('Composite Metric')
+    plt.title('Players: Salary vs Composite Metric')
     plt.tight_layout()
 
     # Show the plot
